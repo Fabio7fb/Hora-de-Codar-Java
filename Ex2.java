cd9be768cde4e9890b62fde69fc84f0cc6c7968c
Fabio Caetano <125493191+Fabio7fb@users.noreply.github.com>
2023-06-30 03:06:13 -0300

    Ex2.java

---
+++ Ex2.java
@@ -0,0 +1,11 @@
+importar  java . . _ Scanner ;
+
+ classe  pública Ex2 {
+
+  public  static  void  principal ( String [] args ) {
+    Scanner  ler = novo  Scanner ( System . in );
+    Sistema . fora . println ( "Informe seu nome: " );
+    String  nome = ler . proximaLinha ();
+    Sistema . fora . println ( "Olá" + nome + "!" );
+  }
+}
